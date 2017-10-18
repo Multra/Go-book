0ae4e998067f8513f3b159c9c1b303616dc086ae
multra <multra@localhost.localdomain>
2017-10-18 16:30:41 -0500

    1.4

---
+++ 1/dup3.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, f := range os.Args[1:] {
+		data, err := ioutil.ReadFile(f)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, l := range strings.Split(string(data), "\n") {
+			counts[l]++
+		}
+	}
+	for l, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, l)
+		}
+	}
+
+}
